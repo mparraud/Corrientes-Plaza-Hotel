3abcac2b5eb31e9690a90e2a63f80ee5a597196e
Miguel Parraud Cordeyro <miguel@parraud.com.ar>
2022-03-30 13:38:51 -0300

    test popup

---
+++ index-pop.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Corrientes Plaza Hotel</title>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2ZTFLHYTPB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2ZTFLHYTPB');
+</script>
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+    <link href="css/hotelCorrientes.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <a href="https://api.whatsapp.com/send?phone=5493794673837" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+<script>
+	$(document).ready(function(){
+		$("#myModal").modal('show');
+	});
+</script>
+</head>
+
+<body id="page-top" class="home">
+
+    <!-- Navigation -->
+    <?php include 'inc_navigation.php';?>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-heading">Un lugar ejecutivo<br>
+                para disfrutar todo el año</div>
+                                <div class="intro-lead-in">En el punto más estratégico de Corrientes</div>
+
+          </div>
+        </div>
+    </header>
+    <div id="myModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+				<p>Reservando por la web, obtené <strong> 5% de descuento.</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<section id="carrouselHome">
+<div class="container">
+    <div class="col-md-12">
+            <div id="myCarousel" class="carousel slide">
+                
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <div class="row">
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_001.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_002.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_003.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_004.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                        </div>
+                        <!--/row-->
+                    </div>
+                    <!--/item-->
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_005.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_006.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_007.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_008.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                        </div>
+                        <!--/row-->
+                    </div>
+                    <!--/item-->
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_009.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_010.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_011.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_012.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                        </div>
+                        <!--/row-->
+                    </div>
+                    <!--/item-->
+
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_013.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_014.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_015.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-3"><a href="#x"><img src="img/hotel/home_slides/slide_016.jpg" alt="Image" class="img-responsive"></a>
+                            </div>
+                        </div>
+                        <!--/row-->
+                    </div>
+                    <!--/item-->
+
+                </div>
+                <!--/carousel-inner--> <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+
+                <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+            <!--/myCarousel-->
+
+    </div>
+</div>
+
+</section>
+
+
+    <!-- Services Section --><!-- Home Banners Section -->
+    <section id="homeBanner">
+    	<div class="container">
+            <div class="row">
+                <div class="col-md-4">
+	                <div class="home_banner01">
+                    	<div class="text1">DESCUBRÍ<br>nuestros servicios</div>
+                        <br>
+                        <div class="text2">
+                        	<span class="glyphicon glyphicon-earphone"></span>
+                              54  0379 4466500<br>y líneas rotativas</div>
+                        </div>
+              </div>
+                <div class="col-md-8">
+                	<div class="home_banner02">...</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">Copyright Corrientes Plaza Hotel - Todos los derechos reservados <?php echo  date("Y");?>. - Términos y condiciones<br/>
+               <img src="img/hotel/gr_footer_data.png" class="dataFiscal">
+              </div>
+              <div class="col-md-4">
+                <img src="img/hotel/gr_footer_smoke-free.png" alt="100% Libre de Humo">
+                <div class="text_smoke-free">Por lo dispuesto en la Ley 26687 y por el bien de su salud este hotel es un espacio libre de humo de cigarrillo y tabaco. Fumar está solo permitido en el exterior del Hotel. Absténgase de fumar en las habitaciones y en los espacios cerrados del Hotel. Al momento de realizar el Check in, se les informará a los huéspedes acerca de esta Política de No Fumador y se le informará del cargo correspondiente que el Hotel cobrará en concepto de limpieza y eliminación de humo, en caso que no se cumpla la política.</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+
+
+    <!-- Custom Carrousel JavaScript -->
+    <script src="js/carrousel_extras.js"></script>
+
+</body>
+
+</html>
